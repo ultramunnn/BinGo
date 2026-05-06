33f179a108ac7cdd466778625c6c0414b59830ee
AdityaWijayanto19 <adityaputraw49@student.ub.ac.id>
2026-05-07 06:46:26 +0700

    feat: finish hero section & usage section

--- frontend/src/layouts/sections/Hero.jsx
+++ frontend/src/layouts/sections/Hero.jsx
@@ -3,6 +3,24 @@ import { motion } from "framer-motion";
 import { Menu } from "lucide-react";
 
 const Hero = () => {
+  // Animasi untuk parent agar child muncul berurutan
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.2, delayChildren: 0.3 },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 30 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8, ease: "easeOut" },
+    },
+  };
+
   return (
     <section className="relative w-full h-screen overflow-hidden font-sans bg-black">
       {/* --- VIDEO BACKGROUND --- */}
