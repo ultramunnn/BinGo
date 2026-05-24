8c6108c6418d0d197919c5cc1437b20170ad69fe
AdityaWijayanto19 <adityaputraw49@student.ub.ac.id>
2026-05-24 20:19:42 +0700

    Revert "feat: implement article detail page with Wikipedia API integration"
    This reverts commit 47d06e805b2076f1b2a1329d79fff1b2d730c138.

--- frontend/src/components/article/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ArticleCard } from "./ArticleCard";
-export { default as LoadingSkeleton } from "./LoadingSkeleton";
-export { default as DetailSkeleton } from "./DetailSkeleton";
-export { EmptyState, ErrorState } from "./EmptyState";
